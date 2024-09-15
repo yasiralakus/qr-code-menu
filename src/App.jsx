28e1f6d237162fc811cebf866cf08a36fc78955f
Yasir Alakus <yasiralakus11@gmail.com>
2024-09-15 19:34:13 +0300

    icecekler eklendi

--- src/App.jsx
+++ src/App.jsx
@@ -12,78 +12,11 @@ export default function App() {
                 <details>
                     <summary>Özel Dürümler</summary>
                     <ul>
-                        <li>Özel Soslu Tavuk Dürüm <span>120₺</span></li>
-                        <li>Meksika Soslu Tavuk Dürüm <span>120₺</span></li>
-                        <li>Cheddar Soslu Tavuk Dürüm <span>120₺</span></li>
-                        <li>Köfte Dürüm <span>160₺</span></li>
-                        <li>Kumru Dürüm <span>120₺</span></li>
-                    </ul>
-                </details>
-
-                <details>
-                    <summary>Gözlemeler</summary>
-                    <ul>
-                        <li>Patatesli Gözleme <span>85₺</span></li>
-                        <li>Peynirli Gözleme <span>85₺</span></li>
-                        <li>Kaşarlı Gözleme <span>85₺</span></li>
-                        <li>Sucuklu Gözleme <span>90₺</span></li>
-                        <li>Sucuklu Kaşarlı Gözleme <span>95₺</span></li>
-                    </ul>
-                </details>
-
-                <details>
-                    <summary>Patsolar</summary>
-                    <ul>
-                        <li>Sade Patso <span>80₺</span></li>
-                        <li>Sosisli Patso <span>100₺</span></li>
-                        <li>Sucuklu Patso <span>100₺</span></li>
-                        <li>Köfteli Patso <span>150₺</span></li>
-                        <li>Patates Tabağı <span>80₺</span></li>
-
-                    </ul>
-                </details>
-
-                <details>
-                    <summary>Tostlar</summary>
-                    <ul>
-                        <li>Kaşarlı Tost <span>80₺</span></li>
-                        <li>Sucuklu Tost <span>80₺</span></li>
-                        <li>Karışık Tost <span>90₺</span></li>
-                        <li>Kavurmalı Tost <span>150₺</span></li>
-                    </ul>
-                </details>
-
-                <details>
-                    <summary>Makarnalar</summary>
-                    <ul>
-                        <li>Kremalı Tavuklu Makarna <span>150₺</span></li>
-                        <li>Köri Soslu Tavuklu Makarna <span>150₺</span></li>
-                        <li>Acılı Kremalı Tavuklu Makarna <span>150₺</span></li>
-                        <li>Soya Soslu Tavuklu Makarna <span>150₺</span></li>
-                        <li>Fettuccine Alfredo <span>180₺</span></li>
-                    </ul>
-                </details>
-
-                <details>
-                    <summary>Pilav Çeşitleri</summary>
-                    <ul>
-                        <li>Nohutlu Pilav <span>110₺</span></li>
-                        <li>Nohutlu Tavuklu Pilav <span>130₺</span></li>
-                        <li>Köfteli Pilav <span>250₺</span></li>
-                        <li>Arnavut Ciğerli Pilav <span>180₺</span></li>
-                        <li>Kavurmalı Pilav <span>250₺</span></li>
-                        <li>Tavuk Soteli Pilav <span>220₺</span></li>
-                    </ul>
-                </details>
-
-                <details>
-                    <summary>Waffle Çeşitleri</summary>
-                    <ul>
-                        <li>Çilekli Waffle <span>160₺</span></li>
-                        <li>Muzlu Waffle <span>160₺</span></li>
-                        <li>Full Karışık Waffle <span>160₺</span></li>
-                        <li>Frambuazlı Waffle <span>180₺</span></li>
-                        <li>Orman Meyveli Waffle <span>180₺</span></li>
+                        <li>Özel Soslu Tavuk Dürüm <span>130₺</span></li>
+                        <li>Meksika Soslu Tavuk Dürüm <span>130₺</span></li>
+                        <li>Cheddar Soslu Tavuk Dürüm <span>130₺</span></li>
+                        <li>Köfte Dürüm <span>170₺</span></li>
+                        <li>Kumru Dürüm <span>130₺</span></li>
                     </ul>
                 </details>
 
@@ -95,64 +28,148 @@ export default function App() {
                                 <summary><i className="fa-solid fa-caret-right"></i> 4'lü Doyuran Menü</summary>
                                 <p>4 Chicken Burger + Mega Boy Patates + 1 L İçecek + 4'lü Soğan Halkası</p>
                             </details>
-                            <span>350₺</span>
+                            <span>360₺</span>
                         </li>
                         <li>
                             <details>
                                 <summary><i className="fa-solid fa-caret-right"></i> İyi Doyduk Menü</summary>
                                 <p>2 Chicken Burger + 2 Köfte Burger + Mega Boy Patates + 1 L İçecek + 4'lü Soğan Halkası</p>
                             </details>
-                            <span>370₺</span>
+                            <span>380₺</span>
                         </li>
                         <li>
                             <details>
                                 <summary><i className="fa-solid fa-caret-right"></i> Hepsinden Olsun Menü</summary>
                                 <p>Çiko Tavuk Burger + Chicken Burger + Köfte Burger + Mega Boy Patates + 1 L İçecek + 4'lü Soğan Halkası</p>
                             </details>
-                            <span>300₺</span>
+                            <span>310₺</span>
                         </li>
                         <li>
                             <details>
                                 <summary><i className="fa-solid fa-caret-right"></i> 2'li Cheeseburger Menü</summary>
                                 <p>2 Cheeseburger + Orta Boy Patates + 1 L İçecek + 4'lü Soğan Halkası</p>
                             </details>
-                            <span>350₺</span>
+                            <span>360₺</span>
                         </li>
                         <li>
                             <details>
                                 <summary><i className="fa-solid fa-caret-right"></i> Efsane İkili Menü</summary>
                                 <p>2 Köfte Burger + Orta Boy Patates + 1 L İçecek + 4'lü Soğan Halkası</p>
                             </details>
-                            <span>360₺</span>
+                            <span>370₺</span>
                         </li>
                         <li>
                             <details>
                                 <summary><i className="fa-solid fa-caret-right"></i> Efsane Üçlü Menü</summary>
                                 <p>3 Köfte Burger + Mega Boy Patates + 1 L İçecek + 4'lü Soğan Halkası</p>
                             </details>
-                            <span>430₺</span>
+                            <span>440₺</span>
                         </li>
                         <li>
                             <details>
                                 <summary><i className="fa-solid fa-caret-right"></i> Hamburger Menü</summary>
                                 <p>Köfte Burger + Küçük Boy Patates + 330 ML İçecek + 4'lü Soğan Halkası</p>
                             </details>
-                            <span>280₺</span>
+                            <span>290₺</span>
                         </li>
                         <li>
                             <details>
                                 <summary><i className="fa-solid fa-caret-right"></i> Çocuk Burger Menü</summary>
                                 <p>Köfte Burger + Küçük Boy Patates + 330 ML İçecek + 4'lü Soğan Halkası</p>
                             </details>
-                            <span>260₺</span>
+                            <span>270₺</span>
                         </li>
                         <li>
                             <details>
                                 <summary><i className="fa-solid fa-caret-right"></i> 2'li Tavuk Burger Menü</summary>
                                 <p>2 Tavuk Burger + Küçük Boy Patates + 330 ML İçecek + 4'lü Soğan Halkası</p>
                             </details>
-                            <span>190₺</span>
+                            <span>200₺</span>
                         </li>
+                    </ul>
+                </details>
+
+                <details>
+                    <summary>Gözlemeler</summary>
+                    <ul>
+                        <li>Patatesli Gözleme <span>95₺</span></li>
+                        <li>Peynirli Gözleme <span>95₺</span></li>
+                        <li>Kaşarlı Gözleme <span>95₺</span></li>
+                        <li>Sucuklu Gözleme <span>100₺</span></li>
+                        <li>Sucuklu Kaşarlı Gözleme <span>105₺</span></li>
+                    </ul>
+                </details>
+
+                <details>
+                    <summary>Patsolar</summary>
+                    <ul>
+                        <li>Sade Patso <span>90₺</span></li>
+                        <li>Sosisli Patso <span>110₺</span></li>
+                        <li>Sucuklu Patso <span>110₺</span></li>
+                        <li>Köfteli Patso <span>160₺</span></li>
+                        <li>Patates Tabağı <span>90₺</span></li>
+
+                    </ul>
+                </details>
+
+                <details>
+                    <summary>Tostlar</summary>
+                    <ul>
+                        <li>Kaşarlı Tost <span>90₺</span></li>
+                        <li>Sucuklu Tost <span>90₺</span></li>
+                        <li>Karışık Tost <span>100₺</span></li>
+                        <li>Kavurmalı Tost <span>160₺</span></li>
+                    </ul>
+                </details>
+
+                <details>
+                    <summary>Makarnalar</summary>
+                    <ul>
+                        <li>Kremalı Tavuklu Makarna <span>160₺</span></li>
+                        <li>Köri Soslu Tavuklu Makarna <span>160₺</span></li>
+                        <li>Acılı Kremalı Tavuklu Makarna <span>160₺</span></li>
+                        <li>Soya Soslu Tavuklu Makarna <span>160₺</span></li>
+                        <li>Fettuccine Alfredo <span>190₺</span></li>
+                    </ul>
+                </details>
+
+                <details>
+                    <summary>Pilav Çeşitleri</summary>
+                    <ul>
+                        <li>Nohutlu Pilav <span>120₺</span></li>
+                        <li>Nohutlu Tavuklu Pilav <span>140₺</span></li>
+                        <li>Köfteli Pilav <span>260₺</span></li>
+                        <li>Arnavut Ciğerli Pilav <span>190₺</span></li>
+                        <li>Kavurmalı Pilav <span>260₺</span></li>
+                        <li>Tavuk Soteli Pilav <span>230₺</span></li>
+                    </ul>
+                </details>
+
+                <details>
+                    <summary>Waffle Çeşitleri</summary>
+                    <ul>
+                        <li>Çilekli Waffle <span>170₺</span></li>
+                        <li>Muzlu Waffle <span>170₺</span></li>
+                        <li>Full Karışık Waffle <span>170₺</span></li>
+                        <li>Frambuazlı Waffle <span>190₺</span></li>
+                        <li>Orman Meyveli Waffle <span>190₺</span></li>
+                    </ul>
+                </details>
+
+                <details>
+                    <summary>İçecekler</summary>
+                    <ul>
+                        <li>Coca Cola 330 ml <span>45₺</span></li>
+                        <li>Fuse Tea Şeftali 330 ml <span>45₺</span></li>
+                        <li>Fuse Tea Limon 330 ml<span>45₺</span></li>
+                        <li>Fanta 330 ml<span>45₺</span></li>
+                        <li>Sprite 330 ml<span>45₺</span></li>
+                        <li>Coca Cola 1 L <span>75₺</span></li>
+                        <li>Coca Cola Zero 1 L <span>75₺</span></li>
+                        <li>Fuse Tea Limon 1 L<span>75₺</span></li>
+                        <li>Fuse Tea Şeftali 1 L <span>75₺</span></li>
+                        <li>Fanta 1 L<span>75₺</span></li>
+                        <li>Sprite 1 L<span>75₺</span></li>
                     </ul>
                 </details>
 
